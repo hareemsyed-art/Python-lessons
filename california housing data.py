19fe010cd2aa5b114044ed31c81da27ba6124a60
hareemsyed-art <hareemsyeds15@gmail.com>
2026-01-30 21:53:37 +0800

    Add files via upload

--- california housing data.py
+++ california housing data.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import matplotlib.pyplot as plt
 a = fetch_california_housing()
 b = pd.DataFrame(a.data, columns = a.feature_names)
 b["Price"] = a.target
@@ -10,9 +11,19 @@ b["Price"] = a.target
 from sklearn.model_selection import train_test_split
 X = b.drop("Price", axis = 1)
 Y = b["Price"]
-X_train, X_test, Y_test, Y_train = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X_train, Y_train)
 Y_pred = model.predict(X_test)
-print(Y_pred)
+print(b.head())
+print(Y_pred[:10])
+from sklearn.metrics import mean_squared_error, r2_score
+print("MSE: ", mean_squared_error(Y_test, Y_pred))
+print("R2: ", r2_score(Y_test, Y_pred))
+
+plt.scatter(Y_test, Y_pred)
+plt.xlabel("The price that the AI has not seen")
+plt.ylabel("The AI's predictions")
+plt.title("House price predictions")
+plt.show()
